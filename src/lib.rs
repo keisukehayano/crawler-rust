af7745806ab24251c07a675da7f65a1f923b1b9c
ohs80340 <ohs80340@ohs80340noMacBook-Air.local>
2022-07-23 14:27:36 +0900

    add error

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ use select::document::Document;
 use select::predicate::Name;
 use url::Url;
 use url::ParseError as UrlParseError;
+use thiserror::Error;
 
 pub struct LinkExtractor {
     client: Client,
@@ -15,24 +16,55 @@ impl LinkExtractor {
         }
     }
 
-    pub fn get_links(&self, url: Url) -> Result<Vec<Url>, eyre::Report> {
-        let response = self.client.get(url).send()?;
+    pub fn get_links(&self, url: Url) -> Result<Vec<Url>, GetLinksError> {
+        log::info!("GET \"{}\"", url);
+        let response = self.client.get(url).send()
+            .map_err(|e| GetLinksError::SendRequest(e))?;
+        let response = response.error_for_status()
+            .map_err(|e| GetLinksError::ServerError(e))?;
         let base_url = response.url().clone();
-        let body = response.text()?;
+        let status = response.status();
+        let body = response.text()
+            .map_err(|e| GetLinksError::ResponseBody(e))?;
         let doc = Document::from(body.as_str());
         let mut links = Vec::new();
+        log::info!("Retrieved {} \"{}\"", status, base_url);
 
         for href in doc.find(Name("a")).filter_map(|a| a.attr("href")) {
             match Url::parse(href) {
-                Ok(url) => { links.push(url); },
-                Err(UrlParseError::RelativeUrlWithoutBase) => {
-                    let url = base_url.join(href)?;
+                Ok(mut url) => {
+                    url.set_fragment(None);
                     links.push(url);
                 },
-                Err(e) => { println!("Error: {}", e); },
+                Err(UrlParseError::RelativeUrlWithoutBase) => {
+                    match base_url.join(href) {
+                        Ok(mut url) => {
+                            url.set_fragment(None);
+                            links.push(url);
+                        },
+                        Err(e) => {
+                            log::warn!("URL join erroe: {}", e);
+                        }
+                    }
+                },
+                Err(e) => {
+                    log::warn!("URL parse error: {}", e);
+                },
             }
         }
         Ok(links)
     }
    
+}
+
+#[derive(Error, Debug)]
+pub enum GetLinksError {
+    #[error("Failed to send a request")]
+    SendRequest(#[source] reqwest::Error),
+    #[error("Failed to read this response body")]
+    ResponseBody(#[source] reqwest::Error),
+    #[error("Failed to make the link absolute")]
+    AbsolutuzuUrl(#[source] url::ParseError),
+    #[error("Server returned an error")]
+    ServerError(#[source] reqwest::Error),
 }
